61f4af4c61ec44014de603b64b682acb00ed71e5
warlock <1163675107@qq.com>
2020-10-20 17:34:36 +0800

    init

---
+++ src/views/MainPage/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    };
+  },
+  components: {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <router-link to="/main/">enter</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'index',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/index.scss";
+
+</style>
