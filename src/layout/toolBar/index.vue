650937c5c16f59835d323686f4feecda4c390def
warlock <1163675107@qq.com>
2020-11-10 17:03:13 +0800

    完成图片插入功能

--- src/layout/toolBar/index.vue
+++ src/layout/toolBar/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="toolBar">
-    <div class="tool-item">
+    <div class="tool-item" @click="choiceTools(0)" :style="changeToolStyle(0)">
       <p>pointer</p>
       <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
     </div>
-    <div class="tool-item">
+    <div class="tool-item" @click="choiceTools(1)" :style="changeToolStyle(1)">
       <p>pen</p>
       <i class="fa fa-pencil" aria-hidden="true"></i>
     </div>
-    <div class="tool-item">
+    <div class="tool-item" @click="choiceTools(2)" :style="changeToolStyle(2)">
       <p>fill</p>
       <i class="fa fa-circle" aria-hidden="true"></i>
     </div>
@@ -16,7 +16,7 @@
       <p>color</p>
       <i class="fa fa-braille" aria-hidden="true"></i>
     </div>
-    <div class="tool-item" id="layer" @click="openLayout">
+    <!-- <div class="tool-item" id="layer" @click="openLayout">
       <p>layer</p>
       <i class="fa fa-arrow-right" aria-hidden="true"></i>
       <div class="switch-box" v-if="layerShow">
@@ -30,16 +30,28 @@
            <i class="fa fa-check" aria-hidden="true" v-if="layer=='image'"></i>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="tool-item">
       <p>showAll</p>
     </div>
     <div class="tool-item">
       <p>hideAnother</p>
     </div>
-    <div class="tool-item" @click='insertImage'>
-      <p>insert</p>
-      <i class="fa fa-file-image-o" aria-hidden="true"></i>
+    <div class="tool-item" id="insertTool">
+      <div id="insertHeader" @click="insertImage">
+        <p>insert</p>
+        <i class="fa fa-file-image-o" aria-hidden="true"></i>
+      </div>
+      <div class="show-box" v-if="imgList.length != 0">
+        <div v-for="(img, index) in imgList" :key="index" class="show-item">
+          <p>{{ img.name }}</p>
+          <i
+            class="fa fa-trash-o"
+            aria-hidden="true"
+            @click="deleteImg(index)"
+          ></i>
+        </div>
+      </div>
     </div>
     <div class="tool-item">
       <p>preview</p>
@@ -57,6 +69,11 @@
         <input type="text" v-model="resolution[1]" />
       </div>
     </div>
+    <input
+      type="file"
+      style="filter:alpha(opacity=0);opacity:0;"
+      ref="imgUpload"
+    />
   </div>
 </template>
 
@@ -65,25 +82,59 @@ export default {
   name: "toolBar",
   data() {
     return {
-      layerShow:false,
-      layer:'draw'
+      layerShow: false,
+      tool: 1,
+      imgList: []
     };
   },
   components: {},
-  mounted() {},
+  mounted() {
+    let input = this.$refs.imgUpload;
+    let self = this;
+    input.addEventListener("change", function() {
+      let file = this.files[0];
+      let reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        function() {
+          self.imgList.push({
+            url: reader.result,
+            name: file.name.slice(-10)
+          });
+          self.$emit("events", 'insert',self.imgList);//似乎没有意义
+        },
+        false
+      );
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    });
+  },
   methods: {
-    importCode:function(){
-      this.$emit('events','importCode')
+    importCode: function() {
+      this.$emit("events", "importCode");
     },
-    insertImage:function(){
-      this.$emit('events','insert')
+    insertImage: function() {
+      let input = this.$refs.imgUpload;
+      this.trigger(input, "click");
+
     },
-    openLayout:function(){
-      this.layerShow=!this.layerShow
+    openLayout: function() {
+      this.layerShow = !this.layerShow;
     },
-    choiceLayout:function(name){
-      this.layer=name;
-       this.$emit('events','choiceLayout',name)
+    choiceTools: function(name) {
+      this.tool = name;
+      if (name <= 1) this.$emit("events", "choiceLayout", name);
+    },
+    changeToolStyle(name) {
+      if (name == this.tool) return `color:rgb(159, 208, 212)`;
+      else return ` `;
+    },
+    trigger(ele, event) {
+      ele[event]();
+    },
+    deleteImg(index) {
+      this.imgList.splice(index,1)
     }
   },
   model: {
@@ -106,11 +157,55 @@ export default {
     margin-top: 1rem;
     display: flex;
     justify-content: space-between;
-            transition:0.05s;
+    transition: 0.05s;
+    &:hover {
+      font-size: 1.25rem;
+      color: rgb(159, 208, 212);
+    }
   }
-  .tool-item:hover{
-        font-size:1.25rem;
-        color:rgb(159, 208, 212)
+  #insertTool {
+    justify-content: flex-start;
+    flex-direction: column;
+    &:hover {
+      font-size: 1rem;
+      color: rgb(255, 255, 255);
+    }
+    #insertHeader {
+      display: flex;
+      justify-content: space-between;
+      &:hover {
+        font-size: 1.25rem;
+        color: rgb(159, 208, 212);
+      }
+    }
+    .show-box {
+      height: auto;
+      cursor: default;
+      margin-top: 0.5rem;
+      padding: 0.3rem 0.3rem 0 0.3rem;
+      border-radius: 10px;
+      color: rgb(52, 73, 94);
+      background: rgba(255, 255, 255, 0.8);
+      .show-item {
+        display: flex;
+        justify-content: space-between;
+        height: 1.5rem;
+        i:hover {
+          cursor: pointer;
+          font-size: 1.25rem;
+          color: rgb(85, 111, 138);
+        }
+        p {
+          font-size: 0.8rem;
+          font-weight: 500;
+          &:hover {
+            cursor: pointer;
+            font-size: 1rem;
+            color: rgb(85, 111, 138);
+          }
+        }
+      }
+    }
   }
   #resolution {
     flex-direction: column;
@@ -122,35 +217,35 @@ export default {
       }
     }
   }
-  #layer{
+  #layer {
     position: relative;
-    .switch-box{
-      width:fit-content;
+    .switch-box {
+      width: fit-content;
       position: absolute;
-      padding:0.3rem;
-      z-index:10;
-      left:100%;
-      color:rgb(52, 73, 94);
+      padding: 0.3rem;
+      z-index: 10;
+      left: 100%;
+      color: rgb(52, 73, 94);
       background: rgba(255, 255, 255, 0.8);
       border-radius: 10px;
-      margin-left:0.3rem;
+      margin-left: 0.3rem;
       font-size: 1rem;
-      header{
-        cursor:auto;
+      header {
+        cursor: auto;
         font-weight: 300;
-        font-size:1.5rem;
+        font-size: 1.5rem;
       }
-      .switch-item{
+      .switch-item {
         margin: 0.5rem 0;
         transition: 0.05s;
         display: flex;
         justify-content: space-between;
       }
-      .switch-item:hover{
-        font-size:1.25rem;
-        color:rgb(85, 111, 138)
+      .switch-item:hover {
+        font-size: 1.25rem;
+        color: rgb(85, 111, 138);
       }
-      .switch-item:nth-last-child(n+1){
+      .switch-item:nth-last-child(n + 1) {
         border-bottom: 1px solid black;
       }
     }
--- src/views/MainPage/index.vue
+++ src/views/MainPage/index.vue
@@ -1,13 +1,34 @@
 <template>
   <div id="main-page">
     <div class="wrapper">
-      <toolBar
-        v-model="resolution"
-        @events="toolEvent"
-      ></toolBar>
+      <toolBar v-model="resolution" @events="toolEvent"></toolBar>
       <div id="draw-block" ref="drawBlock">
-        <canvas id="drawArea" ref="canvas" width="200" height="200"></canvas>
-        <img src="@/assets/logo.png" alt="">
+        <div id="drawLayer" :style="drawlayerStyle">
+          <canvas ref="canvas" width="200" height="200"></canvas>
+        </div>
+        <div id="imageLayer" :style="imagelayerStyle">
+          <vue-draggable-resizable :w="100" :h="100" :parent="true" v-for="(img, index) in imgList" :key="index" >
+            <img
+              :src="img.url"
+              @click="test"
+              class="img-item"
+            />
+          </vue-draggable-resizable>
+          <!-- <vue-draggable-resizable
+            :w="100"
+            :h="100"
+            @dragging="onDrag"
+            @resizing="onResize"
+            :parent="true"
+          >
+            <img
+              src="@/assets/logo.png"
+              alt=""
+              @click="test"
+              class="img-item"
+            />
+          </vue-draggable-resizable> -->
+        </div>
       </div>
     </div>
     <trackBlock
@@ -38,21 +59,87 @@ export default {
       ],
       image: null,
       target: 0,
-      layer:0, //0代表绘图层 1代表图片层
+      layer: 1, //1绘图层 0图片层
+      imgList: null
     };
   },
-  components: {
-    toolBar: Layout.toolBar,
-    trackBlock: Layout.trackBlock
+  computed: {
+    imagelayerStyle: function() {
+      if (this.layer == 1) {
+        return `z-index:10`;
+      } else {
+        return `z-index:20`;
+      }
+    },
+    drawlayerStyle: function() {
+      if (this.layer == 1) {
+        return `z-index:20`;
+      } else {
+        return `z-index:10`;
+      }
+    }
+  },
+  methods: {
+    draw: function(command) {
+      drawTools.draw(this.points, this.target, this.$refs.canvas, command);
+    },
+    frameDelivery: function(m) {
+      this.points.push({
+        order: this.points.length,
+        time: m,
+        data: [],
+        finish: false
+      });
+    },
+    choiceTarget: function(m) {
+      this.target = m;
+    },
+    changeFrame: function(item) {
+      this.points[item.order].time = item.time;
+    },
+    //导出代码
+    importCode: function() {
+      for (const item of this.points) {
+        //去除最后一个点，因为它实际上是废的
+        item.data.pop();
+      }
+      this.points = pointShake(this.points);
+      let code = CreateImportCode({
+        points: this.points,
+        viewX: this.resolution[0],
+        viewY: this.resolution[1]
+      });
+    },
+    insert: function(imgList) {
+      this.imgList = imgList;
+    },
+    choiceLayer: function(name) {
+      this.layer = name;
+    },
+    toolEvent: function(EventName, ...args) {
+      let eventManger = {
+        run: function() {
+          this[EventName](...args);
+        },
+        importCode: this.importCode,
+        insert: this.insert,
+        choiceLayout: this.choiceLayer
+      };
+      eventManger.run();
+    },
+    test: function() {
+      console.log(1);
+    }
   },
   mounted() {
-    let c = this.$refs.drawBlock;
+    let c = this.$refs.canvas;
     let self = this;
     let originX = null;
     let originY = null;
     c.addEventListener(
       "click",
       function(e) {
+        if (self.layer == 0 || e.target.className == "img-item") return; //图片层则不绘画||防误触
         //绘画函数
         if (self.points[self.target].finish == true) return; //如果完成了则直接结束
         let command = null; //设置绘画命令
@@ -80,66 +167,6 @@ export default {
       true
     );
   },
-  methods: {
-    draw: function(command) {
-      drawTools.draw(this.points,this.target, this.$refs.canvas, command);
-    },
-    frameDelivery: function(m) {
-      this.points.push({
-        order: this.points.length,
-        time: m,
-        data: [],
-        finish: false
-      });
-    },
-    choiceTarget: function(m) {
-      this.target = m;
-    },
-    changeFrame: function(item) {
-      this.points[item.order].time = item.time;
-    },
-    //导出代码
-    importCode: function() {
-      for (const item of this.points) { //去除最后一个点，因为它实际上是废的
-        item.data.pop();
-      }
-      this.points = pointShake(this.points);
-      let code = CreateImportCode({
-        points: this.points,
-        viewX: this.resolution[0],
-        viewY: this.resolution[1]
-      });
-    },
-    insert: function() {
-      console.log('insert');
-      // let canvas = this.$refs.canvas;
-      // let context = canvas.getContext("2d");
-      // if (this.image == null) {
-      //   let img = new Image();
-      //   img.src = require("@/assets/logo.png");
-      //   this.image = img;
-      //   img.onload = function() {
-      //     context.drawImage(this, 0, 0);
-      //   };
-      // } else {
-      // context.drawImage(this.image,0,0,300,300,100,100,300,300);
-      // }
-    },
-    choiceLayer:function(name){
-     console.log(name);
-    },
-    toolEvent:function(EventName,...args){
-      let eventManger={
-        run:function(){
-          this[EventName](...args);
-        },
-        importCode:this.importCode,
-        insert:this.insert,
-        choiceLayout:this.choiceLayer
-      }
-      eventManger.run();
-    }
-  },
   watch: {
     resolution: function(val) {
       //设置画布分辨率
@@ -161,6 +188,10 @@ export default {
       //重绘原来画好的图案
       this.draw();
     }
+  },
+  components: {
+    toolBar: Layout.toolBar,
+    trackBlock: Layout.trackBlock
   }
 };
 </script>
@@ -178,12 +209,27 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
-      #drawArea {
-        width: 200px;
-        height: 200px;
+      position: relative;
+      #drawLayer {
         border: 1px solid rgb(139, 139, 139);
         background: rgba(0, 0, 0, 0);
         position: absolute;
+        z-index: 2;
+      }
+      #imageLayer {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        .vdr {
+          border: 1px dashed rgba(255, 255, 255, 0.3);
+        }
+        .img-item {
+          width: 100%;
+          height: 100%;
+        }
       }
     }
   }
